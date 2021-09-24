95a4c9ea81dec306f5d4277d7ea88e67bf8c09d2
AkiraTenchi <34791734+AkiraTenchi@users.noreply.github.com>
2021-09-24 19:56:16 +0200

    implementet timer usage and fixed time formating

--- src/timer.rs
+++ src/timer.rs
@@ -9,7 +9,7 @@ pub struct Timer {
 }
 
 impl Timer {
-    fn new() -> Self {
+    pub(crate) fn new() -> Self {
         let now = Instant::now();
         Self {
             last_instant: now,
@@ -20,7 +20,7 @@ impl Timer {
         }
     }
 
-    fn update(&mut self) {
+    pub(crate) fn update(&mut self) {
         let now = Instant::now();
         self.delta = now - self.last_instant;
         self.last_instant = now;
@@ -28,5 +28,17 @@ impl Timer {
             self.ready = true;
             self.period
         });
+    }
+
+    pub(crate) fn get_delta(&self) -> Duration {
+        self.delta
+    }
+
+    pub(crate) fn get_ready(&self) -> bool {
+        self.ready
+    }
+
+    pub(crate) fn unready(&mut self) {
+        self.ready = false;
     }
 }
